b99a2f00abefa0789c144d587c50a43e8206d391
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-12-14 17:33:46 -0600

    Initial Commit

---
+++ User Defaults/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  User Defaults
+//
+//  Created by Charles Martin Reed on 12/14/18.
+//  Copyright © 2018 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
